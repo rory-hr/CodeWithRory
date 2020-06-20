be7967a9831022d5a57e91a5b6fca4fe4d48bff6
rory-roccio <rory.roccio@gmail.com>
2020-06-19 17:17:36 -0700

    Add basic App container

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+import NavBar from './NavBar';
+
+const Container = styled.div`
+  
+`;
 
 export default () => {
   return (
-    <div>APP</div>
+    <Container>
+      <NavBar />
+    </Container>
   );
 };
