b9060312d95bbd901f392cb0292ef116dfff8f75
rory-roccio <rory.roccio@gmail.com>
2020-06-22 14:53:38 -0700

    Render Image and About components

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import Image from './Image';
+import About from './About';
+
+const Container = styled.main`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default () => {
   return (
-    <div>
-      MAIN
-    </div>
+    <Container>
+      <Image />
+      <About />
+    </Container>
   );
 };
