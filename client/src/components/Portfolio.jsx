0e1a362a58dbda3b03c3e40147537945584f266d
rory-roccio <rory.roccio@gmail.com>
2020-06-23 10:48:41 -0700

    Add basic styles, project data, render projects

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
+import Card from './Card';
+
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  max-width: 425px;
+  width: 100%;
+  margin: 0 5px 0 5px;
+  @media (max-width: 434px) {
+    bottom: 200px;
+  }
+`;
+
+
+const projects = [
+  {
+    title: 'AoE2 Builder',
+    repo: 'https://github.com/rory-roccio/aoe2builder',
+    img: 'aoe2builder.demo',
+    stack: ['React Native', 'React', 'Redux', 'Node', 'Express', 'Postgres'],
+    description: 'A game utility application for planning early game strategies for the popular RTS game Age of Empires II.',
+    link: null,
+  },
+  {
+    title: 'Custom Calendar',
+    repo: 'https://github.com/rory-roccio/aoe2builder',
+    img: 'customCalendar.demo',
+    stack: ['React', 'Node', 'Express', 'MySQL'],
+    description: 'A custom, interactive calendar for restaurant table bookings.',
+    link: null,
+  },
+  {
+    title: 'Denali',
+    repo: 'https://github.com/rory-roccio/aoe2builder',
+    img: 'denali.demo',
+    stack: ['React', 'Node', 'Express', 'Mongo', 'Postgres'],
+    description: 'Back end scaling and optimization project for a restaurant reviews micro-service.',
+    link: null,
+  },
+];
 
 export default () => {
   return (
-    <div>
-      TEST
-    </div>
+    <Container>
+      {projects.map((project, i) => <Card key={i} project={project}/>)}
+    </Container>
   );
 };
