b74c5ed052998baaf3bbb57d8af71801a8071f32
rory-roccio <rory.roccio@gmail.com>
2020-06-23 14:39:35 -0700

    Create basic contact component

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
+import EmailIcon from '@material-ui/icons/Email';
+
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  font-family: 'Roboto', sans-serif;
+  margin-top: 10px;
+  height: 40px;
+  @media (max-width: 434px) {
+    bottom: 200px;
+  }
+`;
+
+const Method = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 30px;
+  width: 120px;
+  border-radius: 5px;
+  align-items: center;
+  justify-content: center;
+  :hover {
+    opacity: 0.8;
+    box-shadow:0px 0px 5px #c7c7c7;
+  }
+`;
+
+const Email = styled.a`
+  margin: 0 0 2px 5px;
+  color: black;
+  text-decoration: none;
+`;
 
 export default () => {
   return (
-    <div>
-      TEST
-    </div>
+    <Container>
+      <Method>
+        <EmailIcon />
+        <Email href="mailto:rory.roccio@gmail.com">Email Me</Email>
+      </Method>
+    </Container>
   );
 };
