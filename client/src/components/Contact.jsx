50c981838c1f693384d2fd4763467f5191c2923f
rory-roccio <rory.roccio@gmail.com>
2020-06-24 15:09:58 -0700

    Adjust size, add transition

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -2,45 +2,47 @@ import React from 'react';
 import styled from 'styled-components';
 import EmailIcon from '@material-ui/icons/Email';
 
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   position: relative;
   font-family: 'Roboto', sans-serif;
   margin-top: 10px;
-  height: 40px;
+  height: 80px;
   @media (max-width: 434px) {
     bottom: 200px;
   }
 `;
 
-const Method = styled.div`
+const Method = styled.a`
   display: flex;
   flex-direction: row;
-  height: 30px;
-  width: 120px;
+  height: 60px;
+  width: 180px;
   border-radius: 5px;
   align-items: center;
   justify-content: center;
+  font-size: 1.5em;
+  text-decoration: none;
+  color: black;
+  transition-property: all;
+  transition-duration: 0.3s;
   :hover {
     opacity: 0.8;
     box-shadow:0px 0px 5px #c7c7c7;
   }
 `;
 
-const Email = styled.a`
+const Email = styled.p`
   margin: 0 0 2px 5px;
-  color: black;
-  text-decoration: none;
 `;
 
 export default () => {
   return (
     <Container>
-      <Method>
+      <Method href="mailto:rory.roccio@gmail.com">
         <EmailIcon />
-        <Email href="mailto:rory.roccio@gmail.com">Email Me</Email>
+        <Email>Email Me</Email>
       </Method>
     </Container>
   );
