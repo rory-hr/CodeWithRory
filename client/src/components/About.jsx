e8f211e491787443d198cfa18f508e6466882d86
rory-roccio <rory.roccio@gmail.com>
2020-06-24 15:09:26 -0700

    Switch to 1 text, adjust size

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -5,81 +5,29 @@ import ButtonPanel from './ButtonPanel';
 const Container = styled.div`
   display: inline-block;
   position: relative;
-  max-width: 200px;
-  width: 100%;
-  height: 300px;
+  max-width: 400px;
+  width: 430px;
+  height: 600px;
   margin: 0 10px 0 10px;
   overflow: hidden;
-  @media (max-width: 434px) {
-    bottom: 200px;
+  @media only screen and (max-width: 863px) {
+    bottom: 497px;
   }
 `;
 
 const Text1 = styled.p`
   position: relative;
   font-family: 'Indie Flower', cursive;
+  font-size: 23px;
   margin: 0 0 0 0;
-  width: 300px;
+  width: 400px;
   z-index: -3;
-  top: 50px;
-  left: 70px;
-  color: #ededed;
-  transform: rotate(5deg);
-
-`;
-
-const Text2 = styled.p`
-  position: relative;
-  font-family: 'Indie Flower', cursive;
-  margin: 0 0 0 0;
-  width: 300px;
+  word-break: break-all;
   color: #c2c2c2;
-  z-index: -2;
-  top: 0px;
-  right: 30px;
-  transform: rotate(23deg);
+
 `;
 
-const Text3 = styled.p`
-  position: relative;
-  font-family: 'Indie Flower', cursive;
-  margin: 0 0 0 0;
-  width: 300px;
-  color: #d6d6d6;
-  z-index: -1;
-  bottom: 400px;
-  right: 20px;
-  transform: rotate(-30deg);
-`;
-
-const text1 = `
-  const { Units, Buildings, Techs } = require('./models');
-  // GET /api/units
-  module.exports.getAllUnits = (callback) => {
-    Units.find((err, results) => {
-      if (err) {
-        callback(err);
-      } else {
-        callback(null, results);
-      }
-    });
-  };
-`;
-
-const text2 = `
-  const express = require('express');
-  const app = express();
-  app.use('/', express.static(path.join(__dirname, 'public/dist')));
-  app.listen(process.env.PORT, (err) => {
-    if (err) {
-      console.error(\`Failed to start server at port: \${PORT});
-    } else {
-      console.log(\`Server listening at port: \${PORT}\`);
-    }
-  });
-`;
-
-const text3 = `
+const text4 = `
   import React from 'react';
   import { useSelect, useDispatch } from 'react-redux';
   import Card from './Card';
@@ -98,14 +46,33 @@ const text3 = `
       </div>
     );
   };
+  const express = require('express');\n
+  const app = express();\n
+  app.use('/', express.static(path.join(__dirname, 'public/dist')));\n
+  app.listen(process.env.PORT, (err) => {\n
+    if (err) {\n
+      console.error(\`Failed to start server at port: \${PORT});\n
+    } else {
+      console.log(\`Server listening at port: \${PORT}\`);\n
+    }\n
+  });\n
+  const { Units, Buildings, Techs } = require('./models');\n
+  // GET /api/units\n
+  module.exports.getAllUnits = (callback) => {\n
+    Units.find((err, results) => {\n
+      if (err) {\n
+        callback(err);\n
+      } else {\n
+        callback(null, results);\n
+      }\n
+    });\n
+  };\n
 `;
 
 export default () => {
   return (
     <Container>
-      <Text1>{text1}</Text1>
-      <Text2>{text2}</Text2>
-      <Text3>{text3}</Text3>
+      <Text1>{text4}</Text1>
       <ButtonPanel />
     </Container>
   );
